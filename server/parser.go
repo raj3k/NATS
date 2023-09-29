db09dd0bc0a7104cc63f8daaf01f3df58564d0a0
raj3k <rajkosky@gmail.com>
2023-09-29 21:23:06 +0200

    feat: returning error, WIP: parsing CONNECT

--- server/parser.go
+++ server/parser.go
@@ -1,5 +1,9 @@
 package server
 
+import (
+	"fmt"
+)
+
 type parserState int
 type parseState struct {
 	state parserState
@@ -14,16 +18,25 @@ const (
 	OP_PO
 	OP_PON
 	OP_PONG
+	OP_C
+	OP_CO
 )
 
 func (c *client) parse(buf []byte) error {
-	for i := 0; i < len(buf); i++ {
-		b := buf[i]
+	var b byte
+	var i int
+	for i = 0; i < len(buf); i++ {
+		b = buf[i]
+
 		switch c.state {
 		case OP_START:
 			switch b {
 			case 'P', 'p':
 				c.state = OP_P
+			case 'C', 'c':
+				c.state = OP_C
+			default:
+				goto parseErr
 			}
 		case OP_P:
 			switch b {
@@ -31,38 +44,61 @@ func (c *client) parse(buf []byte) error {
 				c.state = OP_PI
 			case 'O', 'o':
 				c.state = OP_PO
+			default:
+				goto parseErr
 			}
 		case OP_PI:
 			switch b {
 			case 'N', 'n':
 				c.state = OP_PIN
-			}
-		case OP_PO:
-			switch b {
-			case 'N', 'n':
-				c.state = OP_PON
+			default:
+				goto parseErr
 			}
 		case OP_PIN:
 			switch b {
 			case 'G', 'g':
 				c.state = OP_PING
-			}
-		case OP_PON:
-			switch b {
-			case 'G', 'g':
-				c.state = OP_PONG
+			default:
+				goto parseErr
 			}
 		case OP_PING:
 			switch b {
 			case '\n':
 				c.state = OP_START
 			}
+		case OP_PO:
+			switch b {
+			case 'N', 'n':
+				c.state = OP_PON
+			default:
+				goto parseErr
+			}
+		case OP_PON:
+			switch b {
+			case 'G', 'g':
+				c.state = OP_PONG
+			default:
+				goto parseErr
+			}
 		case OP_PONG:
 			switch b {
 			case '\n':
 				c.state = OP_START
 			}
+		case OP_C:
+			switch b {
+			case 'O', 'o':
+				c.state = OP_CO
+			default:
+				goto parseErr
+			}
+		default:
+			goto parseErr
 		}
 	}
+
 	return nil
+parseErr:
+	err := fmt.Errorf("parser ERROR, state=%d, i=%d", c.state, i)
+	return err
 }
