7118157d7d596965be0e0b3957536851b9dcb0d1
raj3k <rajkosky@gmail.com>
2023-11-16 17:44:57 +0100

    add splitArg func

--- server/util.go
+++ server/util.go
@@ -11,3 +11,25 @@ func parseSize(d []byte) int {
 
 	return size
 }
+
+func splitArg(arg []byte) [][]byte {
+	var args [][]byte
+	start := -1
+	for i, b := range arg {
+		switch b {
+		case ' ', '\t':
+			if start >= 0 {
+				args = append(args, arg[start:i])
+				start = -1
+			}
+		default:
+			if start < 0 {
+				start = i
+			}
+		}
+	}
+	if start >= 0 {
+		args = append(args, arg[start:])
+	}
+	return args
+}
